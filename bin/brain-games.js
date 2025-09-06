3a2d9dc49aded0e8fe7a5102cbf72ef4af296c68
rsln8 <iamrsln23@my.com>
2025-09-06 23:35:29 +0300

    Add ESLint configuration and fix code style

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 ﻿#!/usr/bin/env node
-console.log("Welcome to the Brain Games!");
+import { greetUser } from '../src/cli.js'
+
+greetUser()
---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+﻿import stylistic from '@stylistic/eslint-plugin'
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      ...stylistic.configs.recommended.rules,
+      // Дополнительные правила можно добавить здесь
+    },
+  },
+]
