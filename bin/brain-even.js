7477954d1a5e81115f5bfd08b64bd13470f0e313
rsln8 <iamrsln23@my.com>
2025-09-06 23:45:58 +0300

    Add brain-even game with complete functionality

---
+++ bin/brain-even.js
@@ -0,0 +1,10 @@
+﻿import { greetUser } from '../src/cli.js'
+import playEvenGame from '../src/games/even.js'
+
+const runGame = () => {
+  const name = greetUser()
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+  playEvenGame(name)
+}
+
+runGame()
---
+++ src/games/even.js
@@ -0,0 +1,32 @@
+﻿import readlineSync from 'readline-sync'
+
+const isEven = (number) => number % 2 === 0
+
+const generateQuestion = () => {
+  const number = Math.floor(Math.random() * 100) + 1
+  const correctAnswer = isEven(number) ? 'yes' : 'no'
+  return { number, correctAnswer }
+}
+
+const playEvenGame = (name) => {
+  const roundsCount = 3
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const { number, correctAnswer } = generateQuestion()
+    
+    console.log(`Question: ${number}`)
+    const userAnswer = readlineSync.question('Your answer: ')
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return
+    }
+
+    console.log('Correct!')
+  }
+
+  console.log(`Congratulations, ${name}!`)
+}
+
+export default playEvenGame
